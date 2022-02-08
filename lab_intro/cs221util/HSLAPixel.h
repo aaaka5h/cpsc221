9d9d057c2d128bba5f0777fb1599f394441d6eff
aaaka5h <55672728+aaaka5h@users.noreply.github.com>
2022-02-07 21:19:32 -0800

    Added intro_lab

---
+++ lab_intro/cs221util/HSLAPixel.h
@@ -0,0 +1,33 @@
+/**
+ * @file HSLAPixel.h
+ *
+ * @author CS 221: Data Structures
+ */
+
+#ifndef CS221UTIL_HSLAPIXEL_H
+#define CS221UTIL_HSLAPIXEL_H
+
+#include <string>
+#include <vector>
+#include "HSLAPixel.h"
+
+namespace cs221util {
+  class HSLAPixel {
+  public:
+
+    double h;
+    double l;
+    double s;
+    double a;
+
+    // default constructor
+    HSLAPixel();
+
+    HSLAPixel(double hue, double saturation, double luminance);
+
+    HSLAPixel(double hue, double saturation, double luminance, double alpha);
+  };
+
+}
+
+#endif
