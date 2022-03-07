a46309168a702199dbcfce32ded28d4134a50530
Akash Adhikary <55672728+aaaka5h@users.noreply.github.com>
2022-03-07 02:13:21 -0800

    Update imageTileColorPicker.cpp

--- pa2/imageTileColorPicker.cpp
+++ pa2/imageTileColorPicker.cpp
@@ -7,12 +7,17 @@
 #include "imageTileColorPicker.h"
 
 ImageTileColorPicker::ImageTileColorPicker(PNG& otherimage) {
-  // complete your implementation below
-  
+  img_other = otherimage;
 }
 
 HSLAPixel ImageTileColorPicker::operator()(PixelPoint p) {
-  // complete your implementation below
+  int other_x = p.x;
+  int other_y = p.y;
+
+  // Using while loop with repeated subtraction will go to infinite loop (width/height are unsigned ints)
+  other_x = other_x % img_other.width();
+  other_y = other_y % img_other.height();
   
-  return HSLAPixel(); // REPLACE THIS STUB
+  HSLAPixel* res = img_other.getPixel(other_x, other_y);
+  return *res;
 }
